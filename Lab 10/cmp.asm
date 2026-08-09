77c430135045c1614ed8d31f9bfa554205adc184
Sarah Shahzad <f24605008@nutech.edu.pk>
2026-08-09 07:21:27 +0500

    Lab 10 programs

---
+++ Lab 10/cmp.asm
@@ -0,0 +1,21 @@
+TITLE Demonstrating the Compare Instruction 
+
+.686
+.MODEL flat, stdcall
+.STACK
+INCLUDE Irvine32.inc
+
+.data
+var1 SDWORD -3056
+
+.code
+main PROC
+    mov eax, 0f7893478h
+    mov ebx, 1234F678h
+    cmp al, bl
+    cmp ax, bx
+    cmp eax, ebx
+    cmp eax, var1
+    exit
+main ENDP
+END main
